b0c05a3de0eac5460f733e436f63614ec910ebce
skalyan chakravarthy <skalyan.chakravarthy@publicissapient.com>
2020-05-05 12:33:20 +0530

    added prev

--- pages/index.js
+++ pages/index.js
@@ -23,15 +23,16 @@ class index extends React.Component {
     render() {
         const { stories, statusCode, page } = this.props;
         if (statusCode) {
-            return <Error statusCode={statusCode} />
+            return <Error statusCode={statusCode} /> 
         }
 
         return (
             <div>
                 <StoryList stories={stories ? stories.hits : []} />
                 <div>
-                    <Link href={`/?page=${page + 1}`}><a>More</a></Link>
-                    <div className="stories-page-no" Style="float:right;">Page No: {page}</div>
+                    {page > 1 && <div Style="float:left; padding-left: 5px;"><Link href={`/?page=${page - 1}`}><a>Prev</a></Link> | </div>}
+                    <div Style="float: left; padding-left: 5px;"><Link href={`/?page=${page + 1}`}><a>More</a></Link></div>
+                    <div className="stories-page-no" Style="float:right;padding-right: 5px;">Page No: {page}</div>
                 </div>
 
             </div>
